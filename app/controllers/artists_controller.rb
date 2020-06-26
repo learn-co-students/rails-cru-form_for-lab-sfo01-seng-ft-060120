5e5f56b1612b94d051b0a1e3cb7770c6056e6456
Tisdale511 <tisdalefry@gmail.com>
2020-06-25 19:55:58 -0700

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,29 @@
+class ArtistsController < ApplicationController
+    def index
+
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+        redirect_to @artist
+    end
+
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+
+    def update
+        @artist = Artist.find(params[:id])
+        @artist = Artist.update(params.require(:artist).permit(:name, :bio))
+        redirect_to @artist
+    end
+
+end
